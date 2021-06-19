ec84d19e6265c37184b1154a7fb863c1ca9880e5
Qa70601234 <2798360606@qq.com>
2021-06-19 16:29:01 +0800

    ip代理

--- ceshi.py
+++ ceshi.py
@@ -34,8 +34,9 @@ def over(ip,xieyi_1):#显示活的代理ip
     txt = f'{xieyi_1}.txt'
     open(f"{xieyi_1}.txt", 'w').close()#清空文本
 #    time = "`date '+%T %D'`"
-    os.system(f"echo '#更新时间为：'>{txt}")
-    os.system(f"echo `date '+%T %D'`>>{txt}")
+    time_1 = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())  
+    os.system(f"echo '#更新时间为：{time_1}'>{txt}")
+    #os.system(f"echo -n `date '+%T %D'`>>{txt}")
     #for i in range(ip_num):        
     for i in range(5):        
         try:                
